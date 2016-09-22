d5e0acbe9c345cc9c8979d08dcc508cf116f3218
TechDetective <vishwajeetsinhjadeja4@gmail.com>
2016-09-22 09:54:39 +0530

    Form Validation Added

--- public/js/formValidate.js
+++ public/js/formValidate.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
   $('#register-form').on('submit', function(){
+    var shouldSubmit = false;
+    if(new RegExp('\d{2}[a-zA-Z]{3}\d{4}').test($('input[name="regNo"]').val())){
+      if(new RegExp('[789]\d{9}').test($('input[name="contact"]').val())){
+        return true;
+      } else{
+        $('input[name="contact"]').parents($('.custom-input')).append('<span class="red-text">Invalid Phone Number</span>')
+      }
+    } else{
+      $('input[name="regNo"]').parents($('.custom-input')).append('<span class="red-text">Invalid Registration Number</span>')
+    }
     return false;
   })
 })
