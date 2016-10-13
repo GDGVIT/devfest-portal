df5b4b5cef244795e7fb59f4948aef1934d609b4
ashwini <geek.ashwini@gmail.com>
2016-10-14 04:53:35 +0530

    apis added

--- apis.js
+++ apis.js
@@ -1,17 +1,47 @@
 module.exports = [{
-    name : "dfdsfsd",
-    url : "dssfsdfdsf",
-    logo : "dfddsfsdf"
-},{
-    name : "gfgdfgjghdh",
-    url : "dssfsdfdsf",
-    logo : "dfddsfsdf"
-},{
-    name : "erewrwer45",
-    url : "dssfsdfdsf",
-    logo : "dfddsfsdf"
-},{
-    name : "dgdfhb454e",
-    url : "dssfsdfdsf",
-    logo : "dfddsfsdf"
-}]
+    name : "Github API",
+    url : "https://developer.github.com/v3/",
+    logo : "https://pbs.twimg.com/profile_images/1978599420/electrocat.png"
+},
+{
+    name : "Face plus plus",
+    url : "http://www.faceplusplus.com/uc_home/",
+    logo : "https://avatars3.githubusercontent.com/u/2667818?v=3&s=200"
+},
+{
+    name : "Gitter",
+    url : "https://developer.gitter.im/docs/",
+    logo : "https://avatars2.githubusercontent.com/u/5990364?v=3&s=400"
+},
+{
+    name : "Plino",
+    url : "https://plino.herokuapp.com/",
+    logo : "https://d15n4q3o4x3svq.cloudfront.net/assets/tutorials/curl/api-a397cc184c5622fb5130af1b7baf149d.png"
+},
+{
+    name : "WikiMedia",
+    url : "https://www.mediawiki.org/wiki/API:Main_page",
+    logo : "https://d15n4q3o4x3svq.cloudfront.net/assets/tutorials/curl/api-a397cc184c5622fb5130af1b7baf149d.png"
+},
+{
+    name : "Pokéapi",
+    url : 'http://pokeapi.co/',
+    logo : "https://pbs.twimg.com/profile_images/378800000751640307/4dbd22afba75e8cedf0c680cd38a7876_400x400.png"
+},
+{
+    name : "FourSquare",
+    url : 'http://pokeapi.co/',
+    logo : "https://pbs.twimg.com/profile_images/491965445516193792/Elg-IJOS_400x400.png"
+
+},
+{
+    name : "LinkedIn API",
+    url : 'https://developer.linkedin.com/',
+    logo : "https://pbs.twimg.com/profile_images/614583061448036352/CBpFkPaz.png"
+},
+{
+    name : "Youtube Developer API`",
+    url : 'https://developers.google.com/youtube/',
+    logo : "https://lh5.ggpht.com/jZ8XCjpCQWWZ5GLhbjRAufsw3JXePHUJVfEvMH3D055ghq0dyiSP3YxfSc_czPhtCLSO=w300"
+}
+]
