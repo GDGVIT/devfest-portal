c5f6af780773e0e0194b8945f9363a1a337f85ef
mdakram28 <mdakram28@gmail.com>
2016-10-15 13:49:57 +0530

    Extra apis added

--- apis.js
+++ apis.js
@@ -48,59 +48,109 @@ module.exports.social = [
     {
         name : "500 px",
         url : 'https://developers.500px.com/',
-        logo : "htp://devfest.gdgvitvellore.com/images/api/500px.png"
+        logo : "http://devfest.gdgvitvellore.com/images/api/500px.png"
     },
     {
         name : "Amazon Web Services",
         url : 'https://aws.amazon.com/',
-        logo : "htp://devfest.gdgvitvellore.com/images/api/amazon web services.png"
+        logo : "http://devfest.gdgvitvellore.com/images/api/amazon web services.png"
     },
     {
         name : "Digital Ocean",
         url : 'www.digitalocean.com',
-        logo : "htp://devfest.gdgvitvellore.com/images/api/digital ocean.png"
+        logo : "http://devfest.gdgvitvellore.com/images/api/digital ocean.png"
     },
     {
         name : "Google drive",
         url : 'https://developers.google.com/drive/',
-        logo : "htp://devfest.gdgvitvellore.com/images/api/drive.png"
+        logo : "http://devfest.gdgvitvellore.com/images/api/drive.png"
     },
     {
         name : "Facebook",
         url : 'https://developers.facebook.com',
-        logo : "htp://devfest.gdgvitvellore.com/images/api/facebook.png"
+        logo : "http://devfest.gdgvitvellore.com/images/api/facebook.png"
     },
     {
         name : "Fitbit",
         url : 'https://dev.fitbit.com/',
-        logo : "htp://devfest.gdgvitvellore.com/images/api/fitbit.png"
+        logo : "http://devfest.gdgvitvellore.com/images/api/fitbit.png"
     },
     {
         name : "Flickr",
         url : 'https://www.flickr.com/services/api/',
-        logo : "htp://devfest.gdgvitvellore.com/images/api/flickr.png"
+        logo : "http://devfest.gdgvitvellore.com/images/api/flickr.png"
+    },
+    {
+        name : "Fusion Charts",
+        url : "www.fusioncharts.com/dev/",
+        logo : "http://devfest.gdgvitvellore.com/images/api/fusion charts.png"
+    },
+    {
+        name : "Fusion Tables",
+        url : "https://developers.google.com/fusiontables",
+        logo : "http://devfest.gdgvitvellore.com/images/api/fusion tables.png"
+    },
+    {
+        name : "Google",
+        url : "",
+        logo : "http://devfest.gdgvitvellore.com/images/api/google.png"
+    },
+    {
+        name : "Google Cloud",
+        url : "https://cloud.google.com/compute/docs/api/getting-started",
+        logo : "http://devfest.gdgvitvellore.com/images/api/google cloud.png"
+    },
+    {
+        name : "Google Fit",
+        url : "https://developers.google.com/fit/",
+        logo : "http://devfest.gdgvitvellore.com/images/api/google fit.png"
+    },
+    {
+        name : "Linked In",
+        url : "https://developer.linkedin.com/",
+        logo : "http://devfest.gdgvitvellore.com/images/api/linkedin.png"
+    },
+    {
+        name : "Stack Exchange",
+        url : "https://api.stackexchange.com/",
+        logo : "http://devfest.gdgvitvellore.com/images/api/stack exchange.png"
+    },
+    {
+        name : "Twitter",
+        url : "https://dev.twitter.com/overview/documentation",
+        logo : "http://devfest.gdgvitvellore.com/images/api/twitter.png"
+    },
+    {
+        name : "Youtube",
+        url : "https://developers.google.com/youtube/",
+        logo : "http://devfest.gdgvitvellore.com/images/api/youtube.png"
+    },
+    {
+        name : "Zendesk",
+        url : "https://developer.zendesk.com/rest_api/docs/core/introduction",
+        logo : "http://devfest.gdgvitvellore.com/images/api/zendesk.png"
     }
 ]
 
-module.exports.food = [
-    {
-        name : "food Panda",
-        url : "http://www.foodPanda.com/",
-        logo : "http://www.foodPanda.com/favicon.ico"
-    },
-    {
-        name : "food Panda 2",
-        url : "http://www.foodPanda.com/",
-        logo : "http://www.foodPanda.com/favicon.ico"
-    },
-    {
-        name : "food Panda 3",
-        url : "http://www.foodPanda.com/",
-        logo : "http://www.foodPanda.com/favicon.ico"
-    },
-    {
-        name : "food Panda 4",
-        url : "http://www.foodPanda.com/",
-        logo : "http://www.foodPanda.com/favicon.ico"
-    }
-]
+// module.exports.food = [
+//     {
+//         name : "food Panda",
+//         url : "http://www.foodPanda.com/",
+//         logo : "http://www.foodPanda.com/favicon.ico"
+//     },
+//     {
+//         name : "food Panda 2",
+//         url : "http://www.foodPanda.com/",
+//         logo : "http://www.foodPanda.com/favicon.ico"
+//     },
+//     {
+//         name : "food Panda 3",
+//         url : "http://www.foodPanda.com/",
+//         logo : "http://www.foodPanda.com/favicon.ico"
+//     },
+//     {
+//         name : "food Panda 4",
+//         url : "http://www.foodPanda.com/",
+//         logo : "http://www.foodPanda.com/favicon.ico"
+//     }
+// ]
